80bd2117e8bf1582fab0ca80e6894e1c3f92ba69
Matteo Abis <gmatteo..abis@gmail.com>
2015-03-11 10:16:01 +0100

    check that file exists and import it as np.int32

--- dectris_pilatus_roi.py
+++ dectris_pilatus_roi.py
@@ -1,17 +1,19 @@
 import click
-import numpy
+import numpy as np
 
 @click.command()
-@click.argument("file")
-@click.option("--roi",
-              default=[0, 0, 619, 487],
-              help="list [min_x, min_y, max_x, max_y]")
+@click.argument("file", type=click.Path(exists=True, writable=True))
+@click.option("--roi", "roi",
+              type=int,
+              nargs=4,
+              default=(0, 0, 619, 487),
+              help="tuple (min_x, min_y, max_x, max_y)")
 def crop(file, roi):
     """Crop the image in the raw file file_name to the roi and save it back
 
     :file_name: string
-    :roi: list [min_x, min_y, max_x, max_y]
+    :roi: tuple (min_x, min_y, max_x, max_y)
 
     """
-    input_array = np.fromfile(file).reshape((619, 487))
+    input_array = np.fromfile(file, dtype=np.int32).reshape((619, 487))
     input_array[roi[0]:roi[2], roi[1]:roi[3]].tofile(file)
